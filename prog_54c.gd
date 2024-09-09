aa66ec2061187fff8b6a3b8c1444fe3d312bd861
CaydinH <higbee.c@stu.janesville.k12.wi.us>
2024-09-09 14:35:19 -0500

    9-9

--- prog_54c.gd
+++ prog_54c.gd
@@ -9,3 +9,14 @@ func _on_btn_calc_pressed() -> void:
 	var cir = 2*3.14159*rad
 	$LblArea.text = "%.3f" % area
 	$LblCir.text = "%.3f" % cir
+
+
+func _on_btn_clear_pressed() -> void:
+	$LblArea.text = ""
+	$LblCir.text = ""
+	$TxtRad.text = ""
+	
+
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
