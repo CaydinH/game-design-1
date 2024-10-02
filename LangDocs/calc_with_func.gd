79e01fa7303237f3e294b49ba8ea8063d1fa253c
CaydinH <higbee.c@stu.janesville.k12.wi.us>
2024-10-02 14:38:20 -0500

    blah

--- LangDocs/calc_with_func.gd
+++ LangDocs/calc_with_func.gd
@@ -5,4 +5,32 @@ var num2 = $TxtNum2.text
 
 func add():
 	$LabelOppType.text = "+"
-	
+	$LblOut.text = num1 + num2
+
+func sub():
+	$LabelOppType.text = "-"
+	$LblOut.text = num1 - num2
+
+func mult():
+	$LabelOppType.text = "*"
+	$LblOut.text = num1 * num2
+
+func sqr():
+	$LabelOppType.text = "**"
+	$LblOut.text = num1**num2
+
+func div():
+	$LabelOppType.text = "/"
+	$LblOut.text = num1 / num2
+
+func MOD():
+	$LblOppType.text = "MOD"
+	$LblOut.text = num1 % num2
+
+
+func _on_bth_mult_pressed() -> void:
+	mult()
+
+
+func _on_btn_sub_pressed() -> void:
+	sub()
