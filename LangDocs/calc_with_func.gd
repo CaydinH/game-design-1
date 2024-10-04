64fe769e1d102ec977f6c42c135208a06bc5a78c
CaydinH <higbee.c@stu.janesville.k12.wi.us>
2024-10-04 11:52:37 -0500

    10-4

--- LangDocs/calc_with_func.gd
+++ LangDocs/calc_with_func.gd
@@ -1,31 +1,40 @@
 extends Control
 
-var num1 = $TxtNum1.text
-var num2 = $TxtNum2.text
-
 func add():
-	$LabelOppType.text = "+"
-	$LblOut.text = num1 + num2
+	var num1 = int($TxtNum1.text)
+	var num2 = int($TxtNum2.text)
+	$LblOppType.text = "+"
+	$LblOut.text = "Result: " + str(num1 + num2)
 
 func sub():
-	$LabelOppType.text = "-"
-	$LblOut.text = num1 - num2
+	var num1 = int($TxtNum1.text)
+	var num2 = int($TxtNum2.text)
+	$LblOppType.text = "-"
+	$LblOut.text = "Result: " + str(num1 - num2)
 
 func mult():
-	$LabelOppType.text = "*"
-	$LblOut.text = num1 * num2
+	var num1 = int($TxtNum1.text)
+	var num2 = int($TxtNum2.text)
+	$LblOppType.text = "*"
+	$LblOut.text = "Result: " + str(num1 * num2)
 
 func sqr():
-	$LabelOppType.text = "**"
-	$LblOut.text = num1**num2
+	var num1 = int($TxtNum1.text)
+	var num2 = int($TxtNum2.text)
+	$LblOppType.text = "**"
+	$LblOut.text = "Result: " + str(num1**num2)
 
 func div():
-	$LabelOppType.text = "/"
-	$LblOut.text = num1 / num2
+	var num1 = float($TxtNum1.text)
+	var num2 = float($TxtNum2.text)
+	$LblOppType.text = "/"
+	$LblOut.text = "Result: " + str(num1 / num2)
 
 func MOD():
+	var num1 = int($TxtNum1.text)
+	var num2 = int($TxtNum2.text)
 	$LblOppType.text = "MOD"
-	$LblOut.text = num1 % num2
+	$LblOut.text = "Result: " + str(num1 % num2)
 
 
 func _on_bth_mult_pressed() -> void:
@@ -34,3 +43,30 @@ func _on_bth_mult_pressed() -> void:
 
 func _on_btn_sub_pressed() -> void:
 	sub()
+
+
+func _on_btn_add_pressed() -> void:
+	add()
+
+
+func _on_btn_mod_pressed() -> void:
+	MOD()
+
+
+func _on_btn_div_pressed() -> void:
+	div()
+
+
+func _on_btn_sqr_pressed() -> void:
+	sqr()
+
+
+func _on_btn_clear_pressed() -> void:
+	$LblOppType.text = ""
+	$LblOut.text = "Result:"
+	$TxtNum1.text = ""
+	$TxtNum2.text = ""
+
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
