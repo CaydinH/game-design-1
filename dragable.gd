6e55faf05f8d0724518e54ceab125c69514b3695
CaydinH <higbee.c@stu.janesville.k12.wi.us>
2024-10-09 14:36:37 -0500

    10-9

--- dragable.gd
+++ dragable.gd
@@ -10,7 +10,14 @@ func _process(delta):
 func _on_button_button_down() -> void:
 	dragging = true
 	offset = get_global_mouse_position() - self.global_position
+	freeze = true
 
 
 func _on_button_button_up() -> void:
 	dragging = false
+	freeze = false
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "Plt_Player":
+		body.queue_free()
